c2bfd5ba32a9ac33f93362253ac6b1d67e369d07
RupakDevloper <rupakdevloper@gmail.com>
2024-12-12 17:03:11 +0530

    change page

--- lib/pages/coming_page.dart
+++ lib/pages/coming_page.dart
@@ -7,7 +7,7 @@ class ComingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Coming Soon...the next week"),
+        child: Text("Coming Soon...the next week and stay ture"),
       ),
     );
   }
